34c8983c736748f4cbe620dc28887b79502f79de
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-05-19 17:08:06 +0300

    merge with devel

--- src/main/java/ru/gadjini/any2any/service/keyboard/ReplyKeyboardService.java
+++ src/main/java/ru/gadjini/any2any/service/keyboard/ReplyKeyboardService.java
@@ -11,6 +11,8 @@ import java.util.Locale;
 
 public interface ReplyKeyboardService {
 
+    ReplyKeyboardMarkup languageKeyboard(long chatId, Locale locale);
+
     ReplyKeyboardMarkup getOcrKeyboard(long chatId, Locale locale);
 
     ReplyKeyboardMarkup getMainMenu(long chatId, Locale locale);
