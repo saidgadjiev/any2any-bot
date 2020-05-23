9a32828b96a49b440bba2de1ee434b1748a26dfc
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-05-23 17:46:53 +0300

    Text 2 word and txt

--- src/main/java/ru/gadjini/any2any/service/converter/impl/Text2AnyConverter.java
+++ src/main/java/ru/gadjini/any2any/service/converter/impl/Text2AnyConverter.java
@@ -4,6 +4,9 @@ import com.aspose.pdf.Document;
 import com.aspose.pdf.FontRepository;
 import com.aspose.pdf.Page;
 import com.aspose.pdf.TextFragment;
+import com.aspose.words.DocumentBuilder;
+import com.aspose.words.Font;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,6 +18,9 @@ import ru.gadjini.any2any.service.converter.api.Format;
 import ru.gadjini.any2any.service.converter.api.result.FileResult;
 import ru.gadjini.any2any.utils.Any2AnyFileNameUtils;
 
+import java.awt.*;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -31,7 +37,53 @@ public class Text2AnyConverter extends BaseAny2AnyConverter<FileResult> {
 
     @Override
     public FileResult convert(FileQueueItem fileQueueItem) {
-        return toPdf(fileQueueItem);
+        if (fileQueueItem.getTargetFormat() == Format.PDF) {
+            return toPdf(fileQueueItem);
+        }
+        if (fileQueueItem.getTargetFormat() == Format.TXT) {
+            return toTxt(fileQueueItem);
+        }
+        return toWord(fileQueueItem);
+    }
+
+    private FileResult toTxt(FileQueueItem fileQueueItem) {
+        try {
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+            SmartTempFile result = fileService.createTempFile(Any2AnyFileNameUtils.getFileName(fileQueueItem.getFileName(), "txt"));
+            FileUtils.writeStringToFile(result.getFile(), fileQueueItem.getFileId(), StandardCharsets.UTF_8);
+
+            stopWatch.stop();
+            return new FileResult(result, stopWatch.getTime(TimeUnit.SECONDS));
+        } catch (Exception ex) {
+            throw new ConvertException(ex);
+        }
+    }
+
+    private FileResult toWord(FileQueueItem fileQueueItem) {
+        try {
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+            com.aspose.words.Document document = new com.aspose.words.Document();
+            try {
+                DocumentBuilder documentBuilder = new DocumentBuilder(document);
+                Font font = documentBuilder.getFont();
+                font.setSize(12);
+                font.setColor(Color.BLACK);
+                font.setName("Verdana");
+
+                documentBuilder.write(fileQueueItem.getFileId());
+                SmartTempFile result = fileService.createTempFile(Any2AnyFileNameUtils.getFileName(fileQueueItem.getFileName(), fileQueueItem.getTargetFormat().getExt()));
+                document.save(result.getAbsolutePath());
+
+                stopWatch.stop();
+                return new FileResult(result, stopWatch.getTime(TimeUnit.SECONDS));
+            } finally {
+                document.cleanup();
+            }
+        } catch (Exception ex) {
+            throw new ConvertException(ex);
+        }
     }
 
     private FileResult toPdf(FileQueueItem fileQueueItem) {
