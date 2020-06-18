358f00a038a1ca586850364f4b7f106404988fd2
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-06-18 09:39:18 +0300

    Улучшил преобразование байтов в читаемый размер

--- src/main/java/ru/gadjini/any2any/utils/MemoryUtils.java
+++ src/main/java/ru/gadjini/any2any/utils/MemoryUtils.java
@@ -1,10 +1,23 @@
 package ru.gadjini.any2any.utils;
 
-import org.apache.commons.io.FileUtils;
-
 public class MemoryUtils {
 
     public static String humanReadableByteCount(long bytes) {
-        return FileUtils.byteCountToDisplaySize(bytes);
+        return humanReadableByteCount(bytes, true);
+    }
+
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        long absBytes = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absBytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(absBytes) / Math.log(unit));
+        long th = (long) Math.ceil(Math.pow(unit, exp) * (unit - 0.05));
+        if (exp < 6 && absBytes >= th - ((th & 0xFFF) == 0xD00 ? 51 : 0)) exp++;
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+        if (exp > 4) {
+            bytes /= unit;
+            exp -= 1;
+        }
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
     }
 }
