13b4c1369e7084e4cecc210c3fa827309c7b5e3a
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-06-20 00:33:39 +0300

    revert cancel button to main image editor menu

--- src/main/java/ru/gadjini/any2any/service/image/editor/ImageEditorState.java
+++ src/main/java/ru/gadjini/any2any/service/image/editor/ImageEditorState.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.gadjini.any2any.bot.command.keyboard.ImageEditorCommand;
 import ru.gadjini.any2any.common.MessagesProperties;
+import ru.gadjini.any2any.io.SmartTempFile;
 import ru.gadjini.any2any.model.AnswerCallbackContext;
 import ru.gadjini.any2any.model.EditMediaContext;
 import ru.gadjini.any2any.model.SendFileContext;
@@ -18,6 +19,7 @@ import ru.gadjini.any2any.service.image.resize.ResizeState;
 import ru.gadjini.any2any.service.keyboard.InlineKeyboardService;
 import ru.gadjini.any2any.service.message.MessageService;
 
+import java.io.File;
 import java.util.Locale;
 
 @Component
@@ -80,6 +82,26 @@ public class ImageEditorState implements State {
         commandStateService.setState(chatId, command.getHistoryName(), editorState);
         if (StringUtils.isNotBlank(queryId)) {
             messageService.sendAnswerCallbackQuery(new AnswerCallbackContext(queryId, localisationService.getMessage(MessagesProperties.UPDATE_CALLBACK_ANSWER, locale)));
+        }
+    }
+
+    @Override
+    public void cancel(ImageEditorCommand command, long chatId, String queryId) {
+        EditorState editorState = commandStateService.getState(chatId, command.getHistoryName(), true);
+        if (StringUtils.isNotBlank(editorState.getPrevFilePath())) {
+            String editFilePath = editorState.getCurrentFilePath();
+            editorState.setCurrentFilePath(editorState.getPrevFilePath());
+            editorState.setCurrentFileId(editorState.getPrevFileId());
+            editorState.setPrevFilePath(null);
+            editorState.setPrevFileId(null);
+
+            messageService.editMessageMedia(new EditMediaContext(chatId, editorState.getMessageId(), editorState.getCurrentFileId())
+                    .replyKeyboard(inlineKeyboardService.getImageEditKeyboard(new Locale(editorState.getLanguage()), editorState.canCancel())));
+            commandStateService.setState(chatId, command.getHistoryName(), editorState);
+
+            new SmartTempFile(new File(editFilePath), true).smartDelete();
+        } else {
+            messageService.sendAnswerCallbackQuery(new AnswerCallbackContext(queryId, localisationService.getMessage(MessagesProperties.MESSAGE_CANT_CANCEL_ANSWER, new Locale(editorState.getLanguage()))));
         }
     }
 
