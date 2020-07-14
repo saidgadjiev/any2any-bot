4529872fdbeb066ac84c60c1dacce2792deda22e
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-07-14 22:34:03 +0300

    Update 2

---
+++ env/migration/Update_2.sql
@@ -0,0 +1,13 @@
+INSERT INTO distribution_message(message_ru, message_en) VALUES ('<b>Обновление:</b>
+1. По многочисленным просьбам увеличили лимит на загрузку файлов. Теперь вы можете грузить файлы до <b>1,5 GB</b>. Наслаждайтесь!)
+2. Исправлены некоторые мелкие ошибки.
+
+Наберите команду /start для того, чтобы начать работать с новыми функциями.
+
+По всем возникшим ошибкам, вопросам и предложениям <a href="https://t.me/SmartFileUtilsSupport">Группу поддержки</a>.', '<b>Update:</b>
+1. By popular demand increased the limit on file downloads. Now you can upload files up to <b>1.5 GB</b>. Enjoy!)
+2. Some minor bugs fixed.
+
+Run /start command to start working with the new features.
+
+For any bugs, questions or suggestions, please write to our 24-hour <a href="https://t.me/SmartFileUtilsSupport">Support Group</a>.')
