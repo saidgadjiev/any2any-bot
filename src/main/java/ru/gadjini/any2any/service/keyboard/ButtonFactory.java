47b2df268cee866f54cceb713e8adc25edc8db13
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-07-26 15:34:33 +0300

    Фикс пмд

--- src/main/java/ru/gadjini/any2any/service/keyboard/ButtonFactory.java
+++ src/main/java/ru/gadjini/any2any/service/keyboard/ButtonFactory.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.gadjini.any2any.common.CommandNames;
 import ru.gadjini.any2any.common.MessagesProperties;
-import ru.gadjini.any2any.model.bot.api.object.Message;
 import ru.gadjini.any2any.model.bot.api.object.replykeyboard.buttons.InlineKeyboardButton;
 import ru.gadjini.any2any.request.Arg;
 import ru.gadjini.any2any.request.RequestParams;
