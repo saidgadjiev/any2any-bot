f4e772923d6965acb2cc78fad725746aaa4c1e9c
GadzhievSA <said.gadzhiev@id-mt.ru>
2020-08-05 17:40:57 +0300

    min wait 20 sec max 10 min

--- src/main/java/ru/gadjini/any2any/service/file/FileWorkObject.java
+++ src/main/java/ru/gadjini/any2any/service/file/FileWorkObject.java
@@ -2,12 +2,11 @@ package ru.gadjini.any2any.service.file;
 
 import org.apache.commons.lang3.time.StopWatch;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 public class FileWorkObject {
+
+    private static final int MIN_10 = 10 * 60;
 
     private long chatId;
 
@@ -15,29 +14,13 @@ public class FileWorkObject {
 
     private StopWatch stopWatch = new StopWatch();
 
-    private List<Consumer<FileWorkObject>> stopListeners = new ArrayList<>();
-
     public FileWorkObject(long chatId, FileLimitsDao fileLimitsDao) {
         this.chatId = chatId;
         this.fileLimitsDao = fileLimitsDao;
     }
 
-    public FileWorkObject(long chatId, FileLimitsDao fileLimitsDao, StopWatch stopWatch) {
-        this.chatId = chatId;
-        this.fileLimitsDao = fileLimitsDao;
-        this.stopWatch = stopWatch;
-    }
-
-    public StopWatch getWatch() {
-        return stopWatch;
-    }
-
     public long getChatId() {
         return chatId;
-    }
-
-    public void resume() {
-        stopWatch.resume();
     }
 
     public void start() {
@@ -54,13 +37,14 @@ public class FileWorkObject {
             stopWatch.stop();
 
             fileLimitsDao.setState(chatId, InputFileState.State.COMPLETED);
-            fileLimitsDao.setInputFileTtl(chatId, stopWatch.getTime(TimeUnit.MILLISECONDS), TimeUnit.MILLISECONDS);
-            stopListeners.forEach(fileWorkObjectConsumer -> fileWorkObjectConsumer.accept(this));
-            stopListeners.clear();
-        }
-    }
+            long seconds = stopWatch.getTime(TimeUnit.SECONDS);
+            if (seconds < 20) {
+                seconds  = 20;
+            } else if (seconds > MIN_10) {
+                seconds = MIN_10;
+            }
 
-    public void addStopListener(Consumer<FileWorkObject> callback) {
-        stopListeners.add(callback);
+            fileLimitsDao.setInputFileTtl(chatId, seconds, TimeUnit.SECONDS);
+        }
     }
 }
