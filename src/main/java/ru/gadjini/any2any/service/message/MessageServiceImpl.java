ad73adf841713a3366e73f0c0f8b8f52bf34571e
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-08-16 15:48:10 +0300

    Убрал бешенные логи по проверке участника чата

--- src/main/java/ru/gadjini/any2any/service/message/MessageServiceImpl.java
+++ src/main/java/ru/gadjini/any2any/service/message/MessageServiceImpl.java
@@ -61,8 +61,6 @@ public class MessageServiceImpl implements MessageService {
         try {
             return BooleanUtils.toBoolean(telegramService.isChatMember(isChatMember));
         } catch (Exception ex) {
-            LOGGER.error(ex.getMessage());
-
             return false;
         }
     }
