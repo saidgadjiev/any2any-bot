deaeb8727b900d7b5fe9ecbbdf94c4488931057c
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-11-23 16:34:27 +0300

    producer id

--- env/migration/V34__Downloading_queue.sql
+++ env/migration/V34__Downloading_queue.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS downloading_queue (
     file tg_file,
     producer varchar(64) NOT NULL,
+    producer_id INT NOT NULL,
     progress TEXT,
     file_path VARCHAR(1014),
     delete_parent_dir BOOLEAN NOT NULL DEFAULT FALSE
