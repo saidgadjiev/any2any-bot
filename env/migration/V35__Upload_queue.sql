7107a606ed1d694947150c68893b4fd687d641da
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-11-29 17:07:53 +0300

    uload_queue

--- env/migration/V35__Upload_queue.sql
+++ env/migration/V35__Upload_queue.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS upload_queue
     producer_id INT         NOT NULL,
     progress    TEXT,
     method      VARCHAR(64) NOT NULL,
-    body        text        NOT NULL
+    body        text        NOT NULL,
+    extra       text
 ) inherits (queue);
