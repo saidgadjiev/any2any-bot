64902fe6b294da38d2be091593831bc0dfc408b3
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-03-02 09:49:39 +0300

    Миграции горизонтального масштабирования

---
+++ env/migration/V45__Horizontal_scaling.sql
@@ -0,0 +1,16 @@
+alter table downloading_queue
+    add column if not exists synced_1 boolean not null default false;
+
+alter table downloading_queue
+    add column if not exists synced_2 boolean not null default false;
+
+alter table downloading_queue
+    add column if not exists synced_3 boolean not null default false;
+
+alter table upload_queue
+    add column if not exists synced boolean not null default false;
+
+alter type tg_file add attribute thumb_size BIGINT;
+
+alter table queue
+    ADD COLUMN IF NOT EXISTS server INT NOT NULL DEFAULT 1;
