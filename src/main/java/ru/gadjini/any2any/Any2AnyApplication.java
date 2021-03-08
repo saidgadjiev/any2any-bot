cf1d444e9bbb2bf14fb5247b191fcaafb9f39bc6
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-03-09 00:15:32 +0300

    Фикс сканирование пропертей

--- src/main/java/ru/gadjini/any2any/Any2AnyApplication.java
+++ src/main/java/ru/gadjini/any2any/Any2AnyApplication.java
@@ -4,26 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.gadjini.telegram.smart.bot.commons.property.*;
 import ru.gadjini.telegram.smart.bot.commons.service.LocalisationService;
 
 import java.time.ZoneOffset;
 import java.util.Locale;
 import java.util.TimeZone;
 
-@EnableConfigurationProperties(value = {
-        BotApiProperties.class,
-        FileLimitProperties.class,
-        BotProperties.class,
-        FileManagerProperties.class,
-        DownloadFloodControlProperties.class,
-        UploadFloodControlProperties.class,
-        MediaLimitProperties.class,
-        WebhookProperties.class
-})
+@ConfigurationPropertiesScan("ru.gadjini.telegram")
 @EnableScheduling
 @SpringBootApplication
 @ComponentScan("ru")
