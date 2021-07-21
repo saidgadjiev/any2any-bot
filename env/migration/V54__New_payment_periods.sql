7fcf6fc77cc7e353489ca94d707e1d3c963ea561
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-07-21 14:11:09 +0300

    Новые периоды оплаты

---
+++ env/migration/V54__New_payment_periods.sql
@@ -0,0 +1,11 @@
+INSERT INTO paid_subscription_plan(currency, price, period, active)
+VALUES ('usd', 3, interval '3 months', true);
+
+
+INSERT INTO paid_subscription_plan(currency, price, period, active)
+VALUES ('usd', 6, interval '6 months', true);
+
+
+INSERT INTO paid_subscription_plan(currency, price, period, active)
+VALUES ('usd', 10, interval '12 months', true);
+
