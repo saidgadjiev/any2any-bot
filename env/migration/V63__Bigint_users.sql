4c67773cc10607eb3087893be21a90899377553f
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-12-08 01:25:43 +0300

    Фикс скрипта удаления

--- env/migration/V63__Bigint_users.sql
+++ env/migration/V63__Bigint_users.sql
@@ -4,4 +4,6 @@ ALTER TABLE bulk_distribution ALTER COLUMN user_id TYPE BIGINT;
 
 ALTER TABLE black_list ALTER COLUMN user_id TYPE BIGINT;
 
+DROP TABLE IF EXISTS distribution;
+
 DROP TABLE IF EXISTS distribution_message;
