6c71b84941ad3e1ff888762ec52752fea4ae35d9
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-12-08 00:48:51 +0300

    Bit int user

---
+++ env/migration/V63__Bigint_users.sql
@@ -0,0 +1,7 @@
+ALTER TABLE user_settings ALTER COLUMN user_id TYPE BIGINT;
+
+ALTER TABLE bulk_distribution ALTER COLUMN user_id TYPE BIGINT;
+
+ALTER TABLE black_list ALTER COLUMN user_id TYPE BIGINT;
+
+DROP TABLE IF EXISTS distribution_message;
